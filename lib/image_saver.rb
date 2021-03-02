9b18bbca32db6688d46dd5c55e80653c5c55ff0c
krasno86 <krasno1986@gmail.com>
2021-03-02 16:58:59 +0200

    refactor and add tests

--- lib/image_saver.rb
+++ lib/image_saver.rb
@@ -2,12 +2,15 @@ require 'csv'
 require 'httparty'
 
 class ImageSaver
-  def initialize(text_file)
-    @text_file= text_file
+  attr_reader :text_file, :image_folder
+
+  def initialize(text_file, image_folder = 'images')
+    @text_file = text_file
+    @image_folder = image_folder
   end
 
   def call
-    csv_text = File.read(@text_file)
+    csv_text = File.read(text_file)
     csv = CSV.parse(csv_text, headers: false)
     idx = 0
     csv.each do |row|
@@ -15,9 +18,9 @@ class ImageSaver
       response = HTTParty.get(row.first)
 
       if response.code == 200
-        File.open("images/image_#{idx}.png", 'wb') do |file|
+        File.open(image_folder.concat("/image_#{idx}.png"), 'wb') do |file|
           file.write response
-          response.code
+          puts code: response.code, message: 'Ok!'
         end
       else
         puts code: response.code, message: "Something went wrong: #{response.response}"
--- spec/image_saver_spec.rb
+++ spec/image_saver_spec.rb
@@ -1,18 +1,23 @@
 require 'spec_helper'
-require 'lib/image_saver'
+require_relative '../lib/image_saver'
 
 RSpec.describe ImageSaver do
-  describe 'error' do
-    it 'sums the prices of the items in its line items' do
-      url = 'https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8dXJsfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
-      expect(ImageSaver.new(url).call).to eq(200)
+  describe 'success image saving' do
+    it 'images count in image folder is equal to images' do
+      dir = 'spec/fixtures/test_images'
+      FileUtils.rm_rf(Dir["#{dir}/*"])
+      Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }
+      ImageSaver.new('files/1.txt', dir).call
+      expect(Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }) == 1
     end
   end
 
   describe 'error' do
     it 'sums the prices of the items in its line items' do
-
-      expect(ImageSaver.new('empty_link').call).not_to eq(200)
+      dir = 'spec/fixtures/test_images'
+      FileUtils.rm_rf(Dir["#{dir}/*"])
+      ImageSaver.new('spec/fixtures/files/broken_link.txt', dir).call
+      expect(Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }).to eq(0)
     end
   end
 end
