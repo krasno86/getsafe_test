f3a23a0faec83022ed71ab4347b10dd4dbc7c58b
krasno86 <krasno1986@gmail.com>
2021-03-03 12:27:56 +0200

    add resque

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 ruby '2.6.3'
 
 gem 'down', '~> 5.0'
+gem 'resque', '~> 2.0'
 %w[rspec-core rspec-expectations rspec-mocks rspec-support].each do |lib|
   gem lib, git: "https://github.com/rspec/#{lib}.git", branch: 'main'
 end
--- lib/image_saver.rb
+++ lib/image_saver.rb
@@ -1,7 +1,9 @@
-# frozen_string_literal: true
-
 require 'uri'
 require 'down'
+require 'resque'
+MAX_SIZE = 5 * 1024 * 1024
+READ_TIMEOUT = 5
+MAX_REDIRECTS = 0
 
 class ImageSaver
   attr_reader :text_file, :image_folder
@@ -12,25 +14,32 @@ class ImageSaver
   end
 
   def call
-    text = File.read(text_file).split
     Dir.mkdir('./images') unless File.exist?('./images')
-    text.each do |row|
-      status = Down.open(row).data[:status]
+    File.readlines(text_file).each do |row|
+      row.chomp!
+      if row.slice(URI::DEFAULT_PARSER.make_regexp(%w[http https])) == row
+        begin
+          remote_file = Down.open(row)
+        rescue StandardError => e
+          puts "Rescued: #{e.inspect}"
+          next
+        end
 
-      if row.slice(URI::DEFAULT_PARSER.make_regexp(%w[http https])) == row && status == 200
-        tempfile = Down.download(row, max_size: 5 * 1024 * 1024)
+        tempfile = Down.download(row, max_size: MAX_SIZE, read_timeout: READ_TIMEOUT, max_redirects: MAX_REDIRECTS)
 
-        if tempfile.content_type == 'image/jpeg'
+        if tempfile && remote_file.data[:status] == 200
           File.open(image_folder.concat(row.split('/').last), 'a') do |file|
             file.write tempfile
-            puts code: status, message: 'Ok!'
+            puts code: remote_file.data[:status], message: 'Ok!'
           end
         else
-          p 'Wrong format!'
+          p 'Wrong format or bad connection'
         end
       else
-        p code: status, message: 'Or invalid url'
+        p 'Wrong url format!'
       end
     end
   end
 end
+
+ImageSaver.new('files/1.txt').call
