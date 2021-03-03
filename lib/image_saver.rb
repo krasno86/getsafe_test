414dc47fe929056d14a0ac71b5660a1905c9af46
krasno86 <krasno1986@gmail.com>
2021-03-03 08:32:41 +0200

    refactor

--- lib/image_saver.rb
+++ lib/image_saver.rb
@@ -4,27 +4,31 @@ require 'uri'
 class ImageSaver
   attr_reader :text_file, :image_folder
 
-  def initialize(text_file, image_folder = 'images')
+  def initialize(text_file, image_folder = 'images/')
     @text_file = text_file
     @image_folder = image_folder
   end
 
-  def call
-    text = File.open(text_file).read
-    text.gsub!(/\r\n?/, "\n")
-    idx = 0
-    text.each_line do |row|
-      idx += 1
-      row.scan(URI.regexp)
-      response = HTTParty.get(row)
+  def self.valid_url?(url)
+    url.slice(URI::regexp(%w(http https))) == url
+  end
 
-      if response.code == 200
-        File.open(image_folder.concat("/image_#{idx}.png"), 'wb') do |file|
-          file.write response
-          puts code: response.code, message: 'Ok!'
+  def call
+    text = File.read(text_file).split
+    text.each do |row|
+      if row.slice(URI::regexp(%w(http https))) == row
+        response = HTTParty.get(row)
+
+        if response.code == 200
+          File.open(image_folder.concat(row.split('/').last), 'a') do |file|
+            file.write response
+            puts code: response.code, message: 'Ok!'
+          end
+        else
+          puts code: response.code, message: "Something went wrong: #{response.response}"
         end
       else
-        puts code: response.code, message: "Something went wrong: #{response.response}"
+        p 'invalid url'
       end
     end
   end
--- spec/image_saver_spec.rb
+++ spec/image_saver_spec.rb
@@ -4,17 +4,17 @@ require_relative '../lib/image_saver'
 RSpec.describe ImageSaver do
   describe 'success image saving' do
     it 'images count in image folder is equal to images' do
-      dir = 'spec/fixtures/test_images'
+      dir = 'spec/fixtures/test_images/'
       FileUtils.rm_rf(Dir["#{dir}/*"])
       Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }
       ImageSaver.new('spec/fixtures/files/1.txt', dir).call
-      expect(Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }) == 1
+      expect(Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }) == 3
     end
   end
 
   describe 'error' do
     it 'sums the prices of the items in its line items' do
-      dir = 'spec/fixtures/test_images'
+      dir = 'spec/fixtures/test_images/'
       FileUtils.rm_rf(Dir["#{dir}/*"])
       ImageSaver.new('spec/fixtures/files/broken_link.txt', dir).call
       expect(Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }).to eq(0)
