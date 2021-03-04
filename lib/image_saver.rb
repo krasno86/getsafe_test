77be333145c5680d1eb8d14d2ba52b6c8aa866e1
krasno86 <krasno1986@gmail.com>
2021-03-04 11:43:25 +0200

    refactor

--- lib/image_saver.rb
+++ lib/image_saver.rb
@@ -6,7 +6,7 @@ class ImageSaver
   extend ImageDownloader
   attr_reader :text_file
 
-  def initialize(text_file, _image_folder = 'images/')
+  def initialize(text_file)
     @text_file = text_file
   end
 
